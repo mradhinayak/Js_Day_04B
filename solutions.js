a8fe46c47c16357aa4cdfc6bbc134754926f5c15
Shivam Goswami <mradhinayak@gmail.com>
2025-12-12 13:30:34 +0530

    2nd

--- solutions.js
+++ solutions.js
@@ -1,3 +1,4 @@
+// Solution 1st
 function splitIntoDigit(num) {
     const digits = []
     while(num > 0) {
@@ -8,3 +9,19 @@ function splitIntoDigit(num) {
 }
 
 console.log(splitIntoDigit(1234));
+
+// Solution 2nd
+function reverseDigit(num) {
+    const digits = []
+    while(num > 0) {
+        digits.unshift(num % 10)
+        num = Math.floor(num / 10)
+    }
+    let reversedNumber = 0
+    for(let i = 0; i < digits.length; i++) {
+        reversedNumber += digits[i] * 10 ** i
+    }
+    return reversedNumber
+}
+
+console.log(reverseDigit(1234));
