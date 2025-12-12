38057fbff1e151a55afd02d587f2bd8a3321618b
Shivam Goswami <mradhinayak@gmail.com>
2025-12-12 16:56:19 +0530

    6th

--- solutions.js
+++ solutions.js
@@ -53,3 +53,38 @@ function seprateParts(num) {
     return {wholePart, fractionPart}
 }
 console.log(seprateParts(95.75))
+
+// Solution 5th
+function generateNumberFromWholeAndFractionalDigit(wholeDigits, fractionalDigits) {
+    let wholePart = 0;
+    let fractionPart = 0;
+
+    // wholeDigits.reverse()
+
+    for(let i = 0; i < wholeDigits.length; i++) {
+        wholePart += wholeDigits[i] * 10 ** (wholeDigits.length - i - 1)
+    }
+
+    for(let i = 0; i < fractionalDigits.length; i++) {
+        fractionPart += fractionalDigits[i] * 10 ** -(i + 1)
+    }
+
+    return wholePart + Number(fractionPart.toFixed(10))
+}
+console.log(generateNumberFromWholeAndFractionalDigit([1, 2], [3, 4]))
+
+// Solution 6th
+function isPalindrome(num) {
+    const originalNumber = num;
+    const digits = []
+    while(num > 0) {
+        digits.unshift(num % 10)
+        num = Math.floor(num / 10)
+    }
+    let reversedNumber = 0
+    for(let i = 0; i < digits.length; i++) {
+        reversedNumber += digits[i] * 10 ** i
+    }
+    return reversedNumber === originalNumber
+}
+
